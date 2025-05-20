d10c0013a2174bb65e7ba782a77029bb554aef37
Vladyslav Olishynskyi <vlad.olishynskyi.work@gmail.com>
2025-05-21 01:48:23 +0300

    fix: load configurations in ConfigModule for global access

--- src/queues/worker.module.ts
+++ src/queues/worker.module.ts
@@ -5,10 +5,11 @@ import { MailConfirmationQueuesModule } from './mail-confirmation/mail-confirmat
 import { WeatherDailyQueuesModule } from './mail-weather/weather-daily.queue.module';
 import { WeatherHourlyQueuesModule } from './mail-weather/weather-hourly.queue.module';
 import { ConfigModule } from '@nestjs/config';
+import configurations from '../config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({ isGlobal: true, load: configurations }),
     AppConfigModule,
     MailConfirmationQueuesModule,
     WeatherDailyQueuesModule,
